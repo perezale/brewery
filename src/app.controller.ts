2fa4874e17c93d2dbf6026c790268c6df535f1f1
Alejandro Perez <alecio88@gmail.com>
2021-08-28 17:47:20 -0300

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,12 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  
+  test() {
+        this.client = new OriginSettingClient(client, IngestService.INGEST_ORIGIN);
+        this.httpClient = httpClient;
+        this.clusterService = clusterService;
+        this.threadPool = threadPool;
+        this.settings = clusterService.getSettings();
+  }
 }
