9680c8f2ce861b05bf8f65f0be6e941ccc7b97a1
Alejandro Perez <alecio88@gmail.com>
2021-08-30 07:56:01 -0300

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,18 +5,17 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-
   @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
   
   test() {
-        this.client = new OriginSettingClient(client, IngestService.INGEST_ORIGIN);
-        this.httpClient = httpClient;
-        this.clusterService = clusterService;
-        this.threadPool = threadPool;
-        this.settings = clusterService.getSettings();
+    this.client = new OriginSettingClient(client, IngestService.INGEST_ORIGIN);
+    this.httpClient = httpClient;
+    this.clusterService = clusterService;
+    this.threadPool = threadPool;
+    this.settings = clusterService.getSettings();
   }
 
 }
