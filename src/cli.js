ffac8cb559f9b2a35461ef3cedf5f0fa855fcaab
Alejandro Perez <alecio88@gmail.com>
2021-09-01 09:50:47 -0300

    new test

--- src/cli.js
+++ src/cli.js
@@ -9,11 +9,11 @@ program
 // attach commands
 serveCommand(program);
 
- program
- .command('help <command>')
- .description('Get the help for a specific command')
- .action(function (cmdName) {
-   const cmd = _.find(program.commands, { _name: cmdName });
-   if (!cmd) return program.error(`unknown command ${cmdName}`);
-   cmd.help();
- });
+program
+.command('help <command>')
+.description('Get the help for a specific command')
+.action(function (cmdName) {
+  const cmd = _.find(program.commands, { _name: cmdName });
+  if (!cmd) return program.error(`unknown command ${cmdName}`);
+  cmd.help();
+});
